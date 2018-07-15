bde6ef5b1a734597df6c27af7b8019d294f89b42
GiantSlayer1913 <michaeljaredfisch@gmail.com>
2018-07-14 22:37:08 -0400

    Trying iframe instead of previous code

--- js/stylish-portfolio.js
+++ js/stylish-portfolio.js
@@ -2,32 +2,33 @@
   "use strict"; // Start of use strict
 
   // Play / Pause AUDIO
-  $(document).ready(function() {
-    var audioElement = document.createElement('audio');
-    audioElement.setAttribute('src', 'https://www.youtube.com/embed/btPJPFnesV4');
-    audioElement.addEventListener('ended', function() {
-        this.play();
-    }, false);
-    audioElement.addEventListener("canplay",function(){
-        $("#length").text("Duration:" + audioElement.duration + " seconds");
-        $("#source").text("Source:" + audioElement.src);
-        $("#status").text("Status: Ready to play").css("color","green");
-    });
-    audioElement.addEventListener("timeupdate",function(){
-        $("#currentTime").text("Current second:" + audioElement.currentTime);
-    });
-    $('#play').click(function() {
-        audioElement.play();
-        $("#status").text("Status: Playing");
-    });
-    $('#pause').click(function() {
-        audioElement.pause();
-        $("#status").text("Status: Paused");
-    });
-    $('#restart').click(function() {
-        audioElement.currentTime = 0;
-    });
-});
+  
+//   $(document).ready(function() {
+//     var audioElement = document.createElement('audio');
+//     audioElement.setAttribute('src', 'https://www.youtube.com/embed/btPJPFnesV4');
+//     audioElement.addEventListener('ended', function() {
+//         this.play();
+//     }, false);
+//     audioElement.addEventListener("canplay",function(){
+//         $("#length").text("Duration:" + audioElement.duration + " seconds");
+//         $("#source").text("Source:" + audioElement.src);
+//         $("#status").text("Status: Ready to play").css("color","green");
+//     });
+//     audioElement.addEventListener("timeupdate",function(){
+//         $("#currentTime").text("Current second:" + audioElement.currentTime);
+//     });
+//     $('#play').click(function() {
+//         audioElement.play();
+//         $("#status").text("Status: Playing");
+//     });
+//     $('#pause').click(function() {
+//         audioElement.pause();
+//         $("#status").text("Status: Paused");
+//     });
+//     $('#restart').click(function() {
+//         audioElement.currentTime = 0;
+//     });
+// });
 
   // Closes the sidebar menu
   $(".menu-toggle").click(function(e) {

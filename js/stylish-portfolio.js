5ca42add6a31d03d59e9b6eabfccf6276bd16349
GiantSlayer1913 <michaeljaredfisch@gmail.com>
2018-08-26 19:00:52 -0400

    Added slick carousel to package json, edited css and js and added projects to carousel format in index html

--- js/stylish-portfolio.js
+++ js/stylish-portfolio.js
@@ -1,6 +1,22 @@
 (function($) {
   "use strict"; // Start of use strict
 
+  // On swipe event
+  $('.your-element').on('swipe', function(event, slick, direction){
+    console.log(direction);
+    // left
+  });
+
+  // On edge hit
+  $('.your-element').on('edge', function(event, slick, direction){
+    console.log('edge was hit')
+  });
+
+  // On before slide change
+  $('.your-element').on('beforeChange', function(event, slick, currentSlide, nextSlide){
+    console.log(nextSlide);
+  });
+  
   // Closes the sidebar menu
   $(".menu-toggle").click(function(e) {
     e.preventDefault();
